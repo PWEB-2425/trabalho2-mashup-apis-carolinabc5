2b002390ff1283d903bcfa64c439ee874e18a824
Carolina Cunha <carolinacunha169@gmail.com>
2025-07-18 03:46:18 +0100

    Primeiro commit - estrutura do projeto

---
+++ routes/api.js
@@ -0,0 +1,6 @@
+MONGO_URI=mongodb+srv//carolinacunha30323:#Viana2004@cluster0.cbhj0xy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+PORT=3000
+
+WEATHER_API_KEY=sua_chave_openweather
+PIXABAY_API_KEY=sua_chave_pixabay
+SESSION_SECRET=algum_segredo_forte
